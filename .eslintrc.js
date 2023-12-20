958f18b343b2ca376cf4c3e693065c49e53af495
Mcorduk <om.corduk@gmail.com>
2023-12-20 19:53:52 +0300

    Template created with ESLint, prettier and webpack

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+  extends: ["some-other-config-you-use", "prettier"],
+};
